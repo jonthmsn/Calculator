ebd262335811d58dad6f06a59dee740110720424
BuildTools <unconfigured@null.spigotmc.org>
2019-05-02 19:58:06 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bean
+            int num1 = 0; int num2 = 0;
+
+
+        }
+    }
+}
