620522dc18d1517150a3d7ca1d0b21c7f4684486
BuildTools <unconfigured@null.spigotmc.org>
2019-05-02 20:08:02 -0400

    First check in

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,20 @@ namespace Calculator
             //Bean
             int num1 = 0; int num2 = 0;
 
+            //display title
+            Console.WriteLine("Bean Machine, Calculates your beans...\r");
+            Console.WriteLine("BEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAANNNNN\n");
+
+            //Type how many beans you want to calculate and how you want 
+            Console.WriteLine("Type in the amount of beans you have");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
 
         }
     }
